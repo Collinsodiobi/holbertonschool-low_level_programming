7bcfe47c1919221ab412de2e7349871ca29a2ed8
Collinsodiobi <Collinsodi10@gmail.com>
2026-03-01 15:28:28 +0000

    alphabet program

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "_putchar.c"
 
 /**
  * print_alphabet - prints the alphabet in lowercase
@@ -7,11 +8,8 @@
  */
 void print_alphabet(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar(letter);
-	}
-	_putchar('\n');
+    for (char c = 'a'; c <= 'z'; c++)
+    {
+        _putchar(c);
+    }
 }
