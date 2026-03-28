30b10abde95251e573cff12799627e671213363a
Collinsodiobi <Collinsodi10@gmail.com>
2026-03-28 21:10:31 +0000

    0-memset: implement function that fills memory with a constant byte

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototype for _putchar */
+char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
-
-/* Prototype for _strcat */
-char *_strcat(char *dest, char *src);
 
 #endif /* MAIN_H */
