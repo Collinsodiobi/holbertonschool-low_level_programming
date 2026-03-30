50ecc1c22a16d536ef4ff8260c7c0e69ab7a80dc
Collinsodiobi <Collinsodi10@gmail.com>
2026-03-30 12:43:44 +0000

    Task 1: dlistint_len function implemented

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+/**
+ * struct dlistint_s - doubly linked list node
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ */
 typedef struct dlistint_s
 {
     int n;
@@ -10,6 +18,22 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
+/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
-#endif
+#endif /* LISTS_H */
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
