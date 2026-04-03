9c8c6088a7292f5133222d815e626158442c9a27
Collinsodiobi <Collinsodi10@gmail.com>
2026-04-03 19:26:22 +0000

    Fix Betty style for 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,23 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_dlistint - prints all elements of a doubly linked list
+ * print_dlistint - prints all the elements of a dlistint_t list
  * @h: pointer to the head of the list
- *
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
+	const dlistint_t *current = h;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		count++;
+		current = current->next;
+	}
 
-    return (count);
+	return (count);
 }
