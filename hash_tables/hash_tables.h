a2afc5de7ae9466863a124726ce669927aec6cd2
Collinsodiobi <Collinsodi10@gmail.com>
2026-04-03 13:53:05 +0000

    Add task 6: hash_table_delete function (Betty-compliant, frees memory)

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -35,5 +35,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 /* Task 5 - Print hash table */
-void hash_table_print(const hash_table_t *ht);
+void hash_table_print(const hash_table_t *ht)
+/* Task 6 prototype */
+void hash_table_delete(hash_table_t *ht);
 #endif
