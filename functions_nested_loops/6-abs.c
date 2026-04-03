a83437e5e8ad97d3109f0f4d622ec4b09d5afd3c
Collinsodiobi <Collinsodi10@gmail.com>
2026-04-03 20:38:05 +0000

    Add _abs function

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,13 +2,14 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: the integer to check
+ * @n: the number to compute
  *
- * Return: the absolute value of n
+ * Return: absolute value of n
  */
 int _abs(int n)
 {
 	if (n < 0)
-		n = -n;
+		return (-n);
+
 	return (n);
 }
