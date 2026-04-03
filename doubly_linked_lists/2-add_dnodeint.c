15f7f9c470b206d2dbac73e3653720a3c1eabdfe
Collinsodiobi <Collinsodi10@gmail.com>
2026-04-03 19:35:17 +0000

    Fix Betty style for 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,32 +1,31 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_dnodeint - adds a new node at the beginning of a doubly linked list
- * @head: pointer to the head of the list
- * @n: integer to store in the new node
- *
- * Return: address of the new node, or NULL if it fails
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: double pointer to the head of the list
+ * @n: integer value to add in the new node
+ * Return: address of the new node, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+	dlistint_t *new_node;
 
-    if (!head)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
 
-    if (*head != NULL)
-        (*head)->prev = new_node;
+	if (*head != NULL)
+		(*head)->prev = new_node;
 
-    *head = new_node;
+	*head = new_node;
 
-    return (new_node);
+	return (new_node);
 }
