34e48f1a7eb3314f8674899379faa617b3819fa6
Collinsodiobi <Collinsodi10@gmail.com>
2026-04-03 11:13:46 +0000

    Fix Betty style (tabs, spacing)

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,32 +1,29 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table of given size
- * @size: Size of the array
- * Return: Pointer to the new hash table, or NULL on failure
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to new hash table, or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-    unsigned long int i;
+	hash_table_t *ht;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-    ht->size = size;
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-    {
-        free(ht);
-        return (NULL);
-    }
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-        ht->array[i] = NULL;
-
-    return (ht);
+	return (ht);
 }
