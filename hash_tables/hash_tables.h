92692b03e41bdab19e9790bdb595ee3339599c85
Collinsodiobi <Collinsodi10@gmail.com>
2026-04-03 14:06:30 +0000

    Fix hash_table_print prototype; add task 6 hash_table_delete function

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -3,39 +3,29 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-/**
- * struct hash_node_s - Node of a hash table
- * @key: The key, string
- * @value: The value associated with a key
- * @next: Pointer to the next node
- */
+/* Task 0 */
 typedef struct hash_node_s
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
+    char *key;
+    char *value;
+    struct hash_node_s *next;
 } hash_node_t;
 
-/**
- * struct hash_table_s - Hash table structure
- * @size: Size of the array
- * @array: Array of linked lists
- */
 typedef struct hash_table_s
 {
-	unsigned long int size;
-	hash_node_t **array;
+    unsigned long int size;
+    hash_node_t **array;
 } hash_table_t;
 
-/* Function prototypes */
+/* Task prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-/* Task 5 - Print hash table */
-void hash_table_print(const hash_table_t *ht)
-/* Task 6 prototype */
+void hash_table_print(const hash_table_t *ht);   /* ✅ Corrected prototype */
 void hash_table_delete(hash_table_t *ht);
-#endif
+
+#endif /* HASH_TABLES_H */
