730259abec7ba842d5173256822ada5357120ccd
Collinsodiobi <Collinsodi10@gmail.com>
2026-04-03 23:06:55 +0000

    Add times_table function

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,26 +2,39 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
  */
 void times_table(void)
 {
-	int i, j, prod;
+	int i, j, result;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			prod = i * j;
-			if (j != 0)
+			result = i * j;
+
+			if (j == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (prod < 10)
+
+				if (result < 10)
+				{
 					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
 			}
-			if (prod >= 10)
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
