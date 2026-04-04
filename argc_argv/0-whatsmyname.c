2df49956a062cc613d84f3f2c56128fcf6165672
collinsodiobi <collinsodi10@gmail.com>
2026-04-04 09:51:39 +0000

    Fix Betty style and add README for task 0-whatsmyname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the program name
- * @argc: number of arguments (unused)
- * @argv: array of argument strings
+ * main - prints the name of the program
+ * @argc: argument count (unused)
+ * @argv: argument vector
  *
- * Return: Always 0.
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-    (void)argc;  /* unused variable */
-    printf("%s\n", argv[0]);
-    return (0);
+(void)argc;
+
+printf("%s\n", argv[0]);
+
+return (0);
 }
