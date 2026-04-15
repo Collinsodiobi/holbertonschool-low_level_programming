0e2216c68ea3efbe505281c757caf24aa363e5a5
Collinsodiobi <Collinsodi10@gmail.com>
2026-04-15 13:52:22 +0000

    Task 1: calculator working with menu loop

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,20 +1,59 @@
 #include <stdio.h>
+
 int main(void)
-{    int choice;
-	    while (1)
-    {        printf("Simple Calculator\n");
+{
+    int choice;
+
+    while (1)
+    {
+        printf("Simple Calculator\n");
         printf("1) Add\n");
         printf("2) Subtract\n");
         printf("3) Multiply\n");
         printf("4) Divide\n");
         printf("0) Quit\n");
         printf("Choice: ");
-	        scanf("%d", &choice);
-		        if (choice == 0)
+
+        scanf("%d", &choice);
+
+        if (choice == 0)
         {
             printf("Bye!\n");
             break;
         }
-			    }
+        else if (choice == 1)
+        {
+            int a, b;
+            printf("Enter two numbers: ");
+            scanf("%d %d", &a, &b);
+            printf("Result: %d\n", a + b);
+        }
+        else if (choice == 2)
+        {
+            int a, b;
+            printf("Enter two numbers: ");
+            scanf("%d %d", &a, &b);
+            printf("Result: %d\n", a - b);
+        }
+        else if (choice == 3)
+        {
+            int a, b;
+            printf("Enter two numbers: ");
+            scanf("%d %d", &a, &b);
+            printf("Result: %d\n", a * b);
+        }
+        else if (choice == 4)
+        {
+            int a, b;
+            printf("Enter two numbers: ");
+            scanf("%d %d", &a, &b);
+
+            if (b == 0)
+                printf("Error: division by zero\n");
+            else
+                printf("Result: %d\n", a / b);
+        }
+    }
+
     return 0;
 }
