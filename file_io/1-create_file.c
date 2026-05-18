3349fa807fc63c4a57fe38a119125323a1113269
Collinsodiobi <Collinsodi10@gmail.com>
2026-05-18 08:05:07 +0000

    Add task 1 create_file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - creates a file and writes text inside
+ * @filename: name of the file
+ * @text_content: string to write into the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0;
+	ssize_t written = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+
+		written = write(fd, text_content, len);
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -4,6 +4,10 @@
 #include <stddef.h>
 #include <sys/types.h>
 
+/* Task 0 */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Task 1 */
+int create_file(const char *filename, char *text_content);
 
 #endif
